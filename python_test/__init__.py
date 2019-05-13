8de0e394f0ac6d105f24833d65643eaf5aefb4b7
chunyujin <chunyujin@tencent.com>
2019-05-13 17:37:52 +0800

    补充提交python 基础语法

--- python_test/__init__.py
+++ python_test/__init__.py
@@ -25,28 +25,32 @@ if __name__ == '__main__':
     str = "hello RainMtime"
     # String 类型可以截取一段字符串，以最小波长
     print  str[1:10:2]
-    print str*2
+    print str * 2
+    # 字符串格式化
+    print "my name is %s and age is %d" % ("chunyu", 25)
 
     # List 列表
     print "List 列表"
-    list = ['runoob' ,786,2.23,'john',70.2]
-    tinylist = [123,'john']
+    list = ['runoob', 786, 2.23, 'john', 70.2]
+    tinylist = [123, 'john']
     print list
     print list[0]
     print list[1:3]
     print list[2:]
     print tinylist * 2
     print  list + tinylist
+    del list [0]
+    print list
 
     # tuple 元组
     print "tuple 元组"
-    tuplesample = ('runbo',786,2.23,'john',70.2)
-    tinytuple = (1234,'hon')
+    tuplesample = ('runbo', 786, 2.23, 'john', 70.2)
+    tinytuple = (1234, 'hon')
 
     print tuplesample
     print tuplesample[0]
     print tuplesample[:3]
-    print tinytuple*2
+    print tinytuple * 2
     print tuplesample + tinytuple
 
     # dictionary 字典
@@ -61,17 +65,87 @@ if __name__ == '__main__':
     print dict.keys()
     print dict.values()
 
+    # python 运算符
+    print "python 运算符"
+    a = 10
+    b = 2
+    c = 0
+
+    c = a + b
+    print "a+b的值:", c
+    c = a - b
+    print "a -b 的值：", c
+    # a的b 次方
+    c = a ** b
+    # a//b 取商的余数
+
+    # python 的比较符号（== != <> >= <= < >）
+    a = 21
+    b = 10
+    c = 0
+
+    if a == b:
+        print "1-a 等于b"
+    else:
+        print "1-a 不等于 b"
+
+    if a != b:
+        print "2-a 不等于 b"
+    else:
+        print "2-a 等于b"
+    # 比较2个运算符是否不等于
+    if a <> b:
+        print "3-a 不等于b"
+    else:
+        print "3-a 等于b"
+
+    # python 赋值运算符(=,+=,-=,**=,//=,/=,*=)
 
 
+    # python  位运算符(&,|,^,~,<<,>>2)
 
+    # ptyhon 逻辑运算符(and,or, not)
 
+    # python 成员运算符
 
+    a = 10
+    b = 20
+    list = [1, 2, 3, 4, 5]
 
+    if (a in list):
+        print "1- 变量a 在给定的列表list中"
+    else:
+        print "1 - 变量a 未再给定的列表list中"
 
+    if (a not in list):
+        print "2- 变量a 不在连标list中"
+    else:
+        print "2- 变量a 在列表list中"
 
+    # python 身份验证符（is， not is ）
 
+    # is 是判断两个标示符是不是饮用同一个对象
+    # not is 是相反的意思
 
-    # for 语句
+    a = 20
+    b = 20
+
+    if (a is b):
+        print "a和b 是相同引用"
+    else:
+        print "a 和b 是不相同的引用"
+
+    # python 条件语句
+    if a == b:
+        print "a ==b"
+    elif a > 10:
+        print "a>10"
+    else:
+        print "a!=b"
+
+    # python  循环语句
+    print "python  循环语句-for"
+
     n = 0
     for i in range(5):
 
@@ -81,8 +155,40 @@ if __name__ == '__main__':
         else:
             n += 1
             continue
-
     print n
+
+    for letter in "python":
+        print "当前字母:", letter
+
+    fruits = ['banana', 'apple', 'mango']
+
+    for fruit in fruits:
+        print "当前的水果:", fruit
+
+    print "python  循环语句-while"
+    while n > 0:
+        n -= 1
+        print n
+
+    # python 的pass 语句(pass 是空语句，是为了保持程序结构的完整性，不做任何事情）
+    for letter in 'python':
+        if letter == 'h':
+            pass
+            print '这个是pass快'
+        print '当前字母：', letter
+
+    # python 三引号（triple quotes,允许一个字符串跨多行，字符串可以包含换行符和制表符)
+    print ''' hi,
+    pretty girl!
+    '''
+    print '''hi,\n pretty girl ,have you boy friend?'''
+
+    # python Unicode 字符串
+
+    unicodestr = u'Hello\u0020World!'
+    print  unicodestr
+
+
 
 """
 python 中的多行
